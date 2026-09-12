0cb4844ce512ee4930cfccc716998afca958a1a6
DeibyVejar <xdeibyx@gmail.com>
2026-09-11 23:38:17 -0400

    log out bug fixed once again

--- src/app/NavbaradminComponents/Navbaradmin.Component.ts
+++ src/app/NavbaradminComponents/Navbaradmin.Component.ts
@@ -1,22 +1,27 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router'; // Eliminamos RouterLinkActive
+import { Router, RouterLink } from '@angular/router'; // 1. Importar Router
 import { AuthService } from '../services/auth.service';
 import { AdminUploadComponent } from '../admin-upload/adminuploadcomponents/adminuploadcomponent.component';
 
 @Component({
   selector: 'app-navbar-admin',
   standalone: true,
-  // Eliminamos RouterLinkActive de los imports
   imports: [CommonModule, RouterLink, AdminUploadComponent],
   templateUrl: './Navbaradmin.Component.html',
   styleUrls: ['./Navbaradmin.Component.css']
 })
 export class NavbarAdminComponent {
   public authService = inject(AuthService);
+  private router = inject(Router); // 2. Inyectar el Router
   mostrarModalSubir: boolean = false;
 
   logout() {
-    this.authService.logout();
+    this.authService.logout(); // Borra la sesión
+    
+    // 3. Forzar redirección y recarga para sacar al usuario de inmediato de la vista protegida
+    this.router.navigate(['/login']).then(() => {
+      window.location.reload();
+    });
   }
 }
