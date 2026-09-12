1fbfd8cc43d35a41fc6eeeb17c71ec4e3614ad8b
DeibyVejar <xdeibyx@gmail.com>
2026-09-11 23:32:51 -0400

    log out bug fixed

--- src/app/Navbar.Component/Navbar.Component.ts
+++ src/app/Navbar.Component/Navbar.Component.ts
@@ -1,26 +1,28 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { CartService } from '../services/car.services';
-import { RouterModule } from '@angular/router';
+import { Router, RouterLink } from '@angular/router'; // 1. Importar Router
 import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router';
+import { AdminUploadComponent } from '../admin-upload/adminuploadcomponents/adminuploadcomponent.component';
+
 @Component({
-  selector: 'app-navbar',
+  selector: 'app-navbar-admin',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterModule],
-  templateUrl: './Navbar.Component.html',
-  styleUrl: './Navbar.Component.css'
+  imports: [CommonModule, RouterLink, AdminUploadComponent],
+  templateUrl: './Navbaradmin.Component.html',
+  styleUrls: ['./Navbaradmin.Component.css']
 })
-
-export class NavbarComponent {
-
-   cartService = inject(CartService);
-  authService = inject(AuthService);
-  router = inject(Router);
+export class NavbarAdminComponent {
+  public authService = inject(AuthService);
+  private router = inject(Router); // 2. Inyectar el Router
+  mostrarModalSubir: boolean = false;
 
   logout() {
-    this.authService.logout(); // Este método debe borrar el token/usuario en el servicio
-    this.router.navigate(['/login']); // Redirigimos al usuario
+    // Llamamos al logout del servicio (que borra el localStorage / token)
+    this.authService.logout();
+
+    // Forzamos la redirección inmediata a la tienda o al login
+    this.router.navigate(['/login']).then(() => {
+      window.location.reload(); // Limpia estados residuales de memoria en Angular
+    });
   }
 }
