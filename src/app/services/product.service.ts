f860f888919132a83b92762f267e8919a318b63e
DeibyVejar <xdeibyx@gmail.com>
2026-09-11 22:58:23 -0400

    corregido error headers

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs'; // Combinamos las importaciones
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
@@ -10,25 +10,31 @@ export class ProductService {
   private http = inject(HttpClient);
   private apiUrl = 'https://payiboxgym.pythonanywhere.com/api/products/';
 
-  // 1. Creamos el Subject para notificar cambios
   private productoCreado = new Subject<void>();
   productoCreado$ = this.productoCreado.asObservable();
+
+  // Método auxiliar para obtener los headers con el token de autenticación
+  private getAuthHeaders(): HttpHeaders {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    return new HttpHeaders({
+      'Authorization': `Bearer ${user.token}`
+    });
+  }
 
   getProducts(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
   }
 
   updateProduct(id: number, data: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}${id}/`, data);
+    return this.http.put<any>(`${this.apiUrl}${id}/`, data, { headers: this.getAuthHeaders() });
   }
 
   deleteProduct(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}${id}/`);
+    return this.http.delete<any>(`${this.apiUrl}${id}/`, { headers: this.getAuthHeaders() });
   }
 
-  // 2. Modificamos el método para disparar el aviso (tap)
   createProduct(formData: FormData): Observable<any> {
-    return this.http.post<any>(this.apiUrl, formData).pipe(
+    return this.http.post<any>(this.apiUrl, formData, { headers: this.getAuthHeaders() }).pipe(
       tap(() => this.productoCreado.next())
     );
   }
